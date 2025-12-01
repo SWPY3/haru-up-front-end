8852ed0045932a7239cc5402a89d1a0213d52def
zerohyeon <joyh0808@gmail.com>
2025-12-01 15:02:48 +0900

    refactor: AppCoordinator와 LoginCoordinator 도입

---
+++ HaruUp/Presentation/LoginScreen/LoginViewCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewCoordinator.swift
+//  HaruUp
+//
+//  Created by 조영현 on 12/1/25.
+//
+
+import UIKit
+
+final class LoginCoordinator: Coordinator {
+    // 보통 바꿀 일 없으니 let 추천
+    let navigationController: UINavigationController
+    
+    // child는 기본적으로 빈 배열로 시작
+    var childCoordinators: [any Coordinator] = []
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        // TODO: 나중에 ViewModel, AuthService 적용
+        let loginVC = LoginViewController()
+        navigationController.setViewControllers([loginVC], animated: false)
+        navigationController.setNavigationBarHidden(true, animated: false)
+    }
+}
