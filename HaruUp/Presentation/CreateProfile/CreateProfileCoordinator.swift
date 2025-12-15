7e98a1abd079e96dcd5d667c077e7bc0c457150f
dahun_27 <hdh011027@gmail.com>
2025-12-15 21:24:41 +0900

    feat: 프로필 생성화면 폴더 추가

---
+++ HaruUp/Presentation/CreateProfile/CreateProfileCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  CreateProfileCoordinator.swift
+//  HaruUp
+//
+//  Created by 하다현 on 12/15/25.
+//
+
+import Foundation
---
+++ HaruUp/Presentation/CreateProfile/CreateProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CreateProfileViewModel.swift
+//  HaruUp
+//
+//  Created by 하다현 on 12/15/25.
+//
+
+import Foundation
