fbaa6e7cd350f8945015d2db1c34f24208786d47
dahun_27 <hdh011027@gmail.com>
2025-12-15 21:30:46 +0900

    feat: CreateProfileCoordinator 초기 세팅

--- HaruUp/Presentation/CreateProfile/CreateProfileCoordinator.swift
+++ HaruUp/Presentation/CreateProfile/CreateProfileCoordinator.swift
@@ -5,4 +5,26 @@
 //  Created by 하다현 on 12/15/25.
 //
 
-import Foundation
+import UIKit
+
+
+final class CreateProfileCoordinator: Coordinator {
+    
+    let navigationController: UINavigationController
+
+    var childCoordinators: [any Coordinator] = []
+
+    var onFinish: (() -> Void)?
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let createProfileVC = CreateProfileViewController()
+        let createProfileVM = CreateProfileViewModel()
+        
+        navigationController.setViewControllers([createProfileVC], animated: false)
+    }
+
+}
