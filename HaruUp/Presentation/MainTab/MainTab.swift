96e1e29b4a8f54be1dff5228d4ee94ab95b8e4ae
zerohyeon <joyh0808@gmail.com>
2025-12-16 17:50:06 +0900

    feat: 커스텀 탭바 디자인 기본 틀 적용

---
+++ HaruUp/Presentation/MainTab/MainTab.swift
@@ -0,0 +1,39 @@
+//
+//  MainTab.swift
+//  HaruUp
+//
+//  Created by 조영현 on 12/16/25.
+//
+
+import UIKit
+
+enum MainTab: Int, CaseIterable {
+    case home, history, chart, mypage
+    
+    var title: String {
+        switch self {
+        case .home: return "홈"
+        case .history: return "나의 기록"
+        case .chart: return "차트"
+        case .mypage: return "마이페이지"
+        }
+    }
+    
+    var icon: UIImage? {
+        switch self {
+        case .home: return .iconTabHomeUnselected
+        case .history: return .iconTabHistoryUnselected
+        case .chart: return .iconTabChartUnselected
+        case .mypage: return .iconTabMypageUnselected
+        }
+    }
+    
+    var selectedIcon: UIImage? {
+        switch self {
+        case .home: return .iconTabHomeSelected
+        case .history: return .iconTabHistorySelected
+        case .chart: return .iconTabChartSelected
+        case .mypage: return .iconTabMypageSelected
+        }
+    }
+}
