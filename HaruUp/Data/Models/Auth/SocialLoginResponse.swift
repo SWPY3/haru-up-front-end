6dd07b88a4eea8d57b6faa5199d7846791248bc6
dahun_27 <hdh011027@gmail.com>
2025-12-18 03:32:55 +0900

    fix: AuthAPI- Almofire사용 및 수정

--- HaruUp/Data/Models/Auth/SocialLoginResponse.swift
+++ HaruUp/Data/Models/Auth/SocialLoginResponse.swift
@@ -7,29 +7,42 @@
 
 import Foundation
 
-
-// 로그인 성공 응답
-
-typealias SocialLoginResponse = GenericResponse<AuthData>
-
-
-
-struct AuthData: Codable {
-    let accessToken: String
-    let refreshToken: String?
-    let memberInfo: MemberInfo?
-    // 기존회원
-    let onboardingCompleted: Bool?
-    // 신규회원
-    let onboardingRequired: Bool?
+// 백엔드 응답 DTO
+struct SocialLoginResponseDTO: Decodable {
+    let success: Bool
+    let data: AuthData?
+    let errorMessage: String?
+    
+    struct AuthData: Decodable {
+        let id: Int
+        let name: String?
+        let password: String?
+        let email: String?
+        let loginType: String
+        let snsId: String?
+        let status: String
+        let accessToken: String
+        let refreshToken: String
+    }
 }
 
-struct MemberInfo: Codable {
-    let id: String
-    let name: String?
-    let email: String?
-    let loginType: String
-}
+
+//struct AuthData: Codable {
+//    let accessToken: String
+//    let refreshToken: String?
+//    let memberInfo: MemberInfo?
+//    // 기존회원
+//    let onboardingCompleted: Bool?
+//    // 신규회원
+//    let onboardingRequired: Bool?
+//}
+//
+//struct MemberInfo: Codable {
+//    let id: String
+//    let name: String?
+//    let email: String?
+//    let loginType: String
+//}
 
 
 //struct SocialLoginResponse: Codable {
