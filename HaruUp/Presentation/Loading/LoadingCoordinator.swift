3ae88ccbca5e58945d145cd9f75bb02cf51b8853
dahun_27 <hdh011027@gmail.com>
2025-12-23 05:11:44 +0900

    feat: 기본 파일 세팅

---
+++ HaruUp/Presentation/Loading/LoadingCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  LoadingCoordinator.swift
+//  HaruUp
+//
+//  Created by 하다현 on 12/23/25.
+//
+
+import Foundation
---
+++ HaruUp/Presentation/Loading/LoadingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoadingViewController.swift
+//  HaruUp
+//
+//  Created by 하다현 on 12/23/25.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
