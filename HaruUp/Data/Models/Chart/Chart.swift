773a0045f08294aa19fe424f008e53b6ce09a82e
dahun_27 <hdh011027@gmail.com>
2026-01-12 14:11:40 +0900

    fix: chart model 수정

--- HaruUp/Data/Models/Chart/Chart.swift
+++ HaruUp/Data/Models/Chart/Chart.swift
@@ -7,10 +7,25 @@
 
 import UIKit
 
+struct RankingResponse: Decodable {
+    let success: Bool
+    let data: [ChartItem]? // 실패시 null일 수 있으므로 옵셔널 처리 권장
+    let errorMessage: String?
+}
+
 // 차트 아이템 모델
-struct ChartItem {
+struct ChartItem: Codable {
     let rank: Int
     let title: String
     let tags: [String]
     let count: Int
+    
+    // 서버의 키값과 앱의 변수명을 매핑
+    enum CodingKeys: String, CodingKey {
+        case rank
+        case title = "labelName"
+        case tags = "interestFullPath"
+        case count = "selectionCount"     
+    }
 }
+
